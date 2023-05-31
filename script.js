e2a95cecc2e0563e0dcd0863fa83af3c1cedec98
Azulserafin <azulserafin04@gmail.com>
2023-05-30 21:42:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function showAlert() {
+    alert('No debes hacer todo lo que te dicen, ¡sé tú mismo!');
+}
+
+const form = document.getElementById("myForm")
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+function validateForm() {
+    const input = document.getElementById("email")
+    const inputValue = input.value
+
+    if (!validateEmail(inputValue)) {
+        alert("Por favor ingrese un correo electrónico válido")
+    } else {
+        alert("Correo electrónico enviado correctamente")
+    }
+}
+
+if(form){
+    form.addEventListener("submit", function (lala) {
+        lala.preventDefault()
+        validateForm();
+    })
+}
+
+
+document.querySelector("button.button-menú-toggle")
+.addEventListener("click", function() {
+    document.querySelector(".nav-links").classList.toggle(".nav-links.responsive")
+}
+)
+
+
+   
